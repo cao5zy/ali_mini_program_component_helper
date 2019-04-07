821dc2f5acf76e1fabdbd55dfeeb8b88a75654b4
zongying.cao <zongying.cao@dxc.com>
2019-04-07 20:48:24 +0800

    improve copy all
    Signed-off-by: zongying.cao <zongying.cao@dxc.com>

--- src/copy_all.py
+++ src/copy_all.py
@@ -7,7 +7,7 @@ def copy_directory(source_path, target_path):
     return shutil.copytree(source_path, target_path)
 
 
-def copy_all(project_path, template_name):
+def copy_all(project_path, component_name):
     assert_that(project_path).exists()
     assert_that(is_valid_project(project_path)).is_true()
 
@@ -20,4 +20,4 @@ def copy_all(project_path, template_name):
 
         return cb_init_template
 
-    return copy(get_project_path(project_path, template_name))
+    return copy(get_project_path(project_path, component_name))
